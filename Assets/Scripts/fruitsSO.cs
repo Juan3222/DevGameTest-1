26ee4f0a71a01d7a7fa0c7f7e27f3313aa098d59
Juan Ignacio Torielli Rios <juan3229988@gmail.com>
2024-12-28 15:10:30 -0300

    Version: 0.1.1

---
+++ Assets/Scripts/fruitsSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "nuevaFruta")]
+public class Fruits : ScriptableObject
+{
+     public string fruit;
+     public int size;
+     public Sprite fruitPNG;
+
+}
